8b1249f8cc89800d2e55d4c1de0bd4443bc935b8
Wirblewind <babic.wirbelwind@hotmail.com>
2024-05-02 18:15:02 +0200

    Implemented the basic view datagrid for a task board

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -1,34 +1,42 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
+import CustomStore from 'devextreme/data/custom_store';
+import DataSource from 'devextreme/data/data_source';
 import 'devextreme/data/odata/store';
+import { lastValueFrom } from 'rxjs';
+import { DataService } from 'src/app/shared/services';
 
 @Component({
   templateUrl: 'tasks.component.html'
 })
 
 export class TasksComponent {
-  dataSource: any;
+  // dataSource: any;
   priority: any[];
+  tasks: any[] = [];
+  dataSource: CustomStore;
 
-  constructor() {
-    this.dataSource = {
-      store: {
-        version: 2,
-        type: 'odata',
-        key: 'Task_ID',
-        url: 'https://js.devexpress.com/Demos/DevAV/odata/Tasks'
+  constructor(private httpClient: HttpClient) {
+    this.dataSource = new CustomStore({
+      key: 'id',
+      loadMode: 'raw',
+      
+      load: () => {
+        return lastValueFrom(this.httpClient.get('http://localhost:3000/task-board-1/'));
       },
-      expand: 'ResponsibleEmployee',
-      select: [
-        'Task_ID',
-        'Task_Subject',
-        'Task_Start_Date',
-        'Task_Due_Date',
-        'Task_Status',
-        'Task_Priority',
-        'Task_Completion',
-        'ResponsibleEmployee/Employee_Full_Name'
-      ]
-    };
+      insert: (values) => {
+        return lastValueFrom(this.httpClient.post('http://localhost:3000/task-board-1/', JSON.stringify(values)))
+          .catch(() => { throw 'Insertion failed' });
+      },
+      remove: async (key) => {
+        await lastValueFrom(this.httpClient.delete('http://localhost:3000/task-board-1/' + encodeURIComponent(key)))
+          .catch(() => { throw 'Deletion failed' });
+      },
+      update: (key, values) => {
+        return lastValueFrom(this.httpClient.patch('http://localhost:3000/task-board-1/' + encodeURIComponent(key), values))
+          .catch(() => { throw 'Update failed' });
+      }
+    });
     this.priority = [
       { name: 'High', value: 4 },
       { name: 'Urgent', value: 3 },
